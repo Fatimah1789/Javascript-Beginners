50e68ab4072b6b6d8c5fa0d7d24684e724761f13
FatimahSacha <fatimah.a.sacha@gmail.com>
2021-08-09 13:12:53 +0100

    Create main.js
    Add  variables in javascript using let

---
+++ script/main.js
@@ -0,0 +1,18 @@
+//let age = 23;
+//console.log(age);
+
+let name = "fatimah";
+//console.log(name);
+
+//let message = "hello my name is " + name + " and i am " + age + " years old. ";
+//console.log(message);
+
+// This would create an alert message onto the browser alert(message);
+
+let firstName = "fatimah";
+let lastName = "Sacha";
+//let age = 19;
+let dateOfBirth = "10-05-02"; 
+
+//let welcomeMessage = " welcome " + firstName + " " + lastName + ". Happy " + age + "th birthday! ";
+//console.log(welcomeMessage);
