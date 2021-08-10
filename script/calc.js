ab325671fc38fc4fc41ed9dfb2da5acfb0419a22
FatimahSacha <fatimah.a.sacha@gmail.com>
2021-08-10 10:55:19 +0100

    Update calc.js
    Add event listeners to communicate with the input

--- script/calc.js
+++ script/calc.js
@@ -2,3 +2,13 @@
 let numField1 = document.getElementById('numField1');
 let numField2 = document.getElementById('numField2');
 let resultField = document.getElementById('resultField');
+let form = document.getElementById("xIsWhatPercentOfY");
+
+form.addEventListener('submit', function() {
+    if (!numField1.value || !numField2.value) {
+        alert("Please enter values in the fields");
+    } else {
+        let x = parseFloat(numField1.value);
+        let y = parseFloat(numField2.value);
+    }  
+});
