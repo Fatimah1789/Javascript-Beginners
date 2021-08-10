5b2a8bc6fe3925f87841dee62539aa9b59df3c08
FatimahSacha <fatimah.a.sacha@gmail.com>
2021-08-10 10:32:23 +0100

    Update calc.js
    How to link elements from html to Javascript through ID

--- script/calc.js
+++ script/calc.js
@@ -1,2 +1,4 @@
 // JAVASCRIPT AND THE DOM (building a percentage calculator)
-
+let numField1 = document.getElementById('numField1');
+let numField2 = document.getElementById('numField2');
+let resultField = document.getElementById('resultField');
