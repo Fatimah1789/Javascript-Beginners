82d0f6f7165b99f963ea7036b90f4fd3cb4c5e63
FatimahSacha <fatimah.a.sacha@gmail.com>
2021-09-30 16:56:55 +0100

    Create main.js
    Add variables and different types of data types in javascript

---
+++ javascript 2.0/scripts/main.js
@@ -0,0 +1,57 @@
+// variables:
+let x = 10;
+let y = 50;
+let sum = x * y;
+console.log(sum);
+
+let a = 100;
+let b = 5;
+let total = x / y;
+console.log(total);
+
+let name = "Fatimah Sacha";
+console.log(name);
+
+//DATA TYPES: data is stored inside variables by using keyword 'let'
+
+// STRING data type - sentence wrapped in double quotes 
+let lastName = "Hello my name is Fatimah Sacha and I am 19 years old";
+console.log(lastName);
+
+let footballClub = "Manchester United are going to win the premier league";
+console.log(footballClub);
+
+let weather = "It's a sunny day";
+console.log(weather);
+// if you want quotation mark in your string:
+let weather_a = "It is a \"sunny\" day";
+console.log(weather_a);
+
+//NUMERIC data type - no decimal point
+let num = 1000;
+console.log(num);
+//Decimal point:
+let num1 = 567.78;
+console.log(num1);
+
+//BOOLEAN data type - True or False statement:
+let counter = true;
+let counter1 = false;
+/* if you set a boolean to 1 = true
+   if you set boolean to 0 = false */
+   
+// UNDEFINED data type - when you dont assign a va;ue to the variable 
+let hello;
+
+//NULL data type - data type defined to nothing
+let u = null;
+console.log(u);
+
+// if you add typeof you can see what type of data type it is
+let r = 12345;
+console.log(typeof r);
+
+
+
+
+
